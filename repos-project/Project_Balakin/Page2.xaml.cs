306f8707735ee80a1ada9b445c57b870f2f809be
me1bourne <95063789+me1bourne@users.noreply.github.com>
2023-04-12 13:15:08 +0500

    Add files via upload

---
+++ repos-project/Project_Balakin/Page2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project_Balakin
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            ClassFrame1.frame1.Navigate(new Page3());
+        }
+
+        private void Button_DpiChanged(object sender, DpiChangedEventArgs e)
+        {
+
+        }
+    }
+}
